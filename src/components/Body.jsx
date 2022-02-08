5907749ee8d6319c20dedb1733c9dfb2da51d83d
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-02-08 21:33:59 +0600

    Initial push

---
+++ src/components/Body.jsx
@@ -0,0 +1,182 @@
+import React, {useEffect,useState} from "react";
+import data from "./data";
+import { Button, Form, Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+function Body(){
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    var location="Dhaka";
+    const [isGet, setIsGet]=useState(true);
+    const [tempLocation, setTempLocation]=useState("");
+    
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+    function getData(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=553c6661ad613e2956211da2b465c7ad&units=metric")
+      .then(res => res.json())
+      .then(
+        (result) => {
+            setItems(result);
+            setIsLoaded(true);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+    }
+    useEffect(()=>{
+        if(isGet)
+        {
+            setIsGet(false);
+            getData();
+        } 
+    }, []);
+
+    function changeLocation(event){
+        setIsGet(true);
+        do{
+            getData();
+            setIsGet(false);
+        }while(isGet)
+        event.preventDefault();
+    }
+
+    function change(event){
+      if(tempLocation.length>0)
+      {
+        location=tempLocation;
+        setIsGet(true);
+        do{
+            getData();
+            setIsGet(false);
+        }while(isGet)
+        setTempLocation("");
+      }
+      event.preventDefault();
+    }
+
+    function locationTextChange(event){
+      var value=event.target.value;
+      if(value.length>0)
+      {
+        setTempLocation(value);
+      }
+    }
+
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    const {weather : [{icon, main: main2}]}=items;
+    const {dt, main, wind, clouds, sys, id, name, visibility}=items;
+    var weatherImg="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+    var sunrise=new Date(sys.sunrise*1000).toLocaleTimeString();
+    var sunset=new Date(sys.sunset*1000).toLocaleTimeString();
+    var fetchedTime=new Date(dt*1000).toLocaleTimeString();
+    var hour=new Date(dt*1000).getHours();
+    var backgroundImage="";
+    if(hour>=20 && hour<=22)
+    {
+      backgroundImage="./IMG/night.jpg";
+    }
+    else if(hour>=4 && hour<=5)
+    {
+      backgroundImage="./IMG/dawn.jpg";
+    }
+    else if(hour>=5 && hour<=7)
+    {
+      backgroundImage="./IMG/mid-dawn.jpg";
+    }
+    else if(hour>=8 && hour<=10)
+    {
+      backgroundImage="./IMG/morning.jpg";
+    }
+    else if(hour>=11 && hour<=12)
+    {
+      backgroundImage="./IMG/mid-morning.jpg";
+    }
+    else if(hour>=13 && hour<=14)
+    {
+      backgroundImage="./IMG/noon.jpg";
+    }
+    else if(hour>=15 && hour<=16)
+    {
+      backgroundImage="./IMG/mid-noon.jpg";
+    }
+    else if(hour>=17 && hour<=18)
+    {
+      backgroundImage="./IMG/afternoon.jpg";
+    }
+    else if(hour>=19 && hour<20)
+    {
+      backgroundImage="./IMG/evening.jpg";
+    }
+    else{
+      backgroundImage="./IMG/mid-night.jpg";
+    }
+    return (
+    <div style={{backgroundImage: "url("+backgroundImage+")",
+   /* Full height */
+  height: "100%",
+
+/* Center and scale the image nicely */
+backgroundPosition: "center",
+backgroundRepeat: "no-repeat",
+backgroundSize: "cover"}}>
+    <Header />
+      <Container>
+      <Col>
+      <Form>
+        <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Accurate Location"
+          aria-label="Location"
+          aria-describedby="basic-addon2"
+          name="locationText"
+          onChange={locationTextChange}
+          value={tempLocation}
+        />
+        <Button onClick={change} type="submit" variant="outline-primary" id="location">
+          Get
+        </Button>
+        </InputGroup>
+        </Form>
+      </Col>
+      <Col>
+        <h1>{name}</h1>
+        <h2>{main.temp}°C</h2>
+        <p>Feel's like {main.feels_like}°C</p>
+        <img src={weatherImg}/>
+        <h4>{main2}</h4>
+        <h5>Pressure: {main.pressure}hPa</h5>
+        <h5>Humidity: {main.humidity}%</h5>
+        <h5>Wind: {wind.speed} Km/pH at {wind.deg}°</h5>
+        <h5>Cloudiness: {clouds.all}%</h5>
+        <h5>Sunrise: {sunrise}</h5>
+        <h5>Sunset: {sunset}</h5>
+        <h3>Country: {sys.country}</h3>
+        <p>Last fetched at: {fetchedTime}</p>
+      </Col>
+      <Button onClick={changeLocation} variant="outline-primary">Refresh</Button>
+    </Container>
+    <Footer />
+    </div>
+    );
+  }
+  
+}
+
+export default Body;
---
+++ src/components/data.js
@@ -0,0 +1,45 @@
+const data={
+    "coord": {
+        "lon": 90.4074,
+        "lat": 23.7104
+    },
+    "weather": [
+        {
+            "id": 721,
+            "main": "Haze",
+            "description": "haze",
+            "icon": "50d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 22.99,
+        "feels_like": 22.62,
+        "temp_min": 22.99,
+        "temp_max": 22.99,
+        "pressure": 1015,
+        "humidity": 49
+    },
+    "visibility": 3500,
+    "wind": {
+        "speed": 3.09,
+        "deg": 280
+    },
+    "clouds": {
+        "all": 0
+    },
+    "dt": 1644304044,
+    "sys": {
+        "type": 1,
+        "id": 9145,
+        "country": "BD",
+        "sunrise": 1644280559,
+        "sunset": 1644320950
+    },
+    "timezone": 21600,
+    "id": 1185241,
+    "name": "Dhaka",
+    "cod": 200
+}
+
+export default data;
